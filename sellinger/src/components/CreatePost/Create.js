ee5686ecca4dffa6a31c1b7e8b71e10b3af96400
Hristian86 <59567820+Hristian86@users.noreply.github.com>
2020-07-14 21:54:21 +0300

    Resolve merge

--- sellinger/src/components/CreatePost/Create.js
+++ sellinger/src/components/CreatePost/Create.js
@@ -1,17 +1,7 @@
 import React, { Component } from 'react';
 import Validate from './Validate';
-<<<<<<< HEAD
 import Successully from './Successully';
 import CreateQuery from '../FirebaseDB/Query-Service/CreateQuery';
-=======
-<<<<<<< HEAD
-import Successully from './Successully';
-import CreateQuery from '../FirebaseDB/Query-Service/CreateQuery';
-=======
-import CreateQuery from '../FirebaseDB/CreateQuery';
-import Successully from './Successully';
->>>>>>> 868b3e90d38c0db41408702ba47cb3f39818520f
->>>>>>> 074ebfa281304b683946ad49ceb3c9990f28e65a
 
 var onSuccessing = "Success";
 
@@ -31,10 +21,6 @@ export default class Create extends Component {
         const phone = e.target.phone.value;
         const content = e.target.content.value;
         const image = e.target.image.value;
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 074ebfa281304b683946ad49ceb3c9990f28e65a
         const price = e.target.price.value;
         console.log(price);
 
@@ -43,17 +29,6 @@ export default class Create extends Component {
 
             let query = new CreateQuery();
             const responce = query.Create(subject,phone,content,image,price);
-<<<<<<< HEAD
-=======
-=======
-
-        const isNotValidate = Validate(subject,phone,content,image);
-        if (!isNotValidate) {
-
-            let query = new CreateQuery();
-            const responce = query.Create(subject,phone,content,image);
->>>>>>> 868b3e90d38c0db41408702ba47cb3f39818520f
->>>>>>> 074ebfa281304b683946ad49ceb3c9990f28e65a
             if (responce === onSuccessing) {
                 
                 this.setState({procesing:true});
@@ -83,15 +58,6 @@ export default class Create extends Component {
                             <input type="text" className="form-control" placeholder="subject" name="subject" />
                             <span id="subject" ></span>
                         </div>
-<<<<<<< HEAD
-                        
-=======
-<<<<<<< HEAD
-                        
-=======
-
->>>>>>> 868b3e90d38c0db41408702ba47cb3f39818520f
->>>>>>> 074ebfa281304b683946ad49ceb3c9990f28e65a
                         <div className="form-group col-md-6">
                             <label >Phone number</label>
                             <input type="text" className="form-control" placeholder="phone number" name="phone" />
@@ -110,18 +76,11 @@ export default class Create extends Component {
                         <input type="text" className="form-control" placeholder="image link" name="image" />
                         <span id="image" ></span>
                     </div>
-
-<<<<<<< HEAD
                     <div className="form-group w-25">
                         {/* <div className="form-check">
 =======
-<<<<<<< HEAD
                     <div className="form-group w-25">
                         {/* <div className="form-check">
-=======
-                    <div className="form-group">
-                        <div className="form-check">
->>>>>>> 868b3e90d38c0db41408702ba47cb3f39818520f
 >>>>>>> 074ebfa281304b683946ad49ceb3c9990f28e65a
                             <input className="form-check-input" type="checkbox" id="gridCheck" />
                             <label className="form-check-label" >
@@ -135,12 +94,6 @@ export default class Create extends Component {
                         <label >Price</label>
                         <input type="text" className="form-control" placeholder="price" name="price" />
                         <span id="price" ></span>
-<<<<<<< HEAD
-=======
-=======
-                        </div>
->>>>>>> 868b3e90d38c0db41408702ba47cb3f39818520f
->>>>>>> 074ebfa281304b683946ad49ceb3c9990f28e65a
                     </div>
                     <button type="submit" className="btn btn-primary">Create post</button>
                 </form>
--- sellinger/src/components/CreatePost/Validate.js
+++ sellinger/src/components/CreatePost/Validate.js
@@ -1,9 +1,5 @@
 import React from 'react';
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 074ebfa281304b683946ad49ceb3c9990f28e65a
 const Validate = (subject,phone,content,image,price) => {
     let chek = false;
     if (isNaN(price) || price.length < 1) {
@@ -11,33 +7,19 @@ const Validate = (subject,phone,content,image,price) => {
         error.innerHTML = "The input is not a number";
         chek = true;
     }
-<<<<<<< HEAD
-=======
-=======
-const Validate = (subject,phone,content,image) => {
-    let chek = false;
->>>>>>> 868b3e90d38c0db41408702ba47cb3f39818520f
->>>>>>> 074ebfa281304b683946ad49ceb3c9990f28e65a
     if (subject.length < 3) {
         let error = document.getElementById('subject');
         error.innerHTML = "Length must be more that 2 symbols";
         chek = true;
     }
-<<<<<<< HEAD
     if (phone.length < 3 || isNaN(phone)) {
         let error = document.getElementById('phone');
         error.innerHTML = "The input is not a phone number";
-=======
-<<<<<<< HEAD
+        chek = true;
+    }
     if (phone.length < 3 || isNaN(phone)) {
         let error = document.getElementById('phone');
         error.innerHTML = "The input is not a phone number";
-=======
-    if (phone.length < 3) {
-        let error = document.getElementById('phone');
-        error.innerHTML = "Length must be more that 2 symbols";
->>>>>>> 868b3e90d38c0db41408702ba47cb3f39818520f
->>>>>>> 074ebfa281304b683946ad49ceb3c9990f28e65a
         chek = true;
     }
     if (content.length < 3) {
@@ -53,7 +35,7 @@ const Validate = (subject,phone,content,image) => {
     if (!chek) {
         return false;
     }
+    
     return true;
 }
-
 export default Validate
