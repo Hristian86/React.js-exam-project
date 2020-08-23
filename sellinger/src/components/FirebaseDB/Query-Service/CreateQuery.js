e3072474a34105cf5ce092d337c6d3ea6a92f1bd
Hristian86 <59567820+Hristian86@users.noreply.github.com>
2020-08-23 09:52:40 +0300

    Small change

--- sellinger/src/components/FirebaseDB/Query-Service/CreateQuery.js
+++ sellinger/src/components/FirebaseDB/Query-Service/CreateQuery.js
@@ -1,23 +1,29 @@
-import React,{Component} from 'react'
+import React, { Component } from 'react'
 
 export default class CreateQuery extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {}
     }
 
-    Create = async (model,url,token) => {
-        return await fetch(url("create"), {
-            "method": "POST",
-            "headers": {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`
-            }, body: JSON.stringify(model)
-        })
-            .then(res => res.json())
-            .catch(err => {
-                console.log(err);
-            });
+    Create = async (model, url, token) => {
+        try {
+
+            return await fetch(url("create"), {
+                "method": "POST",
+                "headers": {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                }, body: JSON.stringify(model)
+            })
+                .then(res => res.json())
+                .catch(err => {
+                    console.log(err);
+                });
+
+        } catch (e) {
+            console.log(e);
+        }
     }
 }
